39dd7f9995a8dc631cc522c1cfce878af8bb5699
KalinHar <kalix@abv.bg>
2022-04-19 14:53:21 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>REACTION TIMER</h1>
+  <button @click="start" :disabled="isPlayng">play</button>
+  <Block v-if="isPlayng" :delay="delay" @end="endGame" />
+  <Result v-if="showResults" :score="score" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from './components/Block.vue'
+import Result from './components/Result.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { Block, Result },
+  data() {
+    return {
+      isPlayng: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000
+      this.isPlayng = true
+      this.showResults = false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime
+      this.isPlayng = false
+      this.showResults =true
+    }
+  },
+
 }
 </script>
 
@@ -20,7 +42,22 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+button {
+  background: rgb(30, 118, 90);
+  color: white;
+  border: none;
+  padding: 8px 16px;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,34 @@
+<template>
+  <p>Reaction time - {{ score }} ms.</p>
+  <p class="rank">Rank: {{ rank }}</p>
+</template>
+
+<script>
+export default {
+    props: ['score'],
+    data() {
+        return {
+            rank: null,
+        }
+    },
+    mounted() {
+        if (this.score < 150) {
+            this.rank = 'Flash'
+        }else if (this.score < 250) {
+            this.rank = 'Ninja'
+        }else if (this.score < 450) {
+            this.rank = 'Normal'
+        }else {
+            this.rank = 'Snail ...'
+        }
+    }
+}
+</script>
+
+<style>
+    .rank {
+        font-size: 1.4rem;
+        color: blue;
+        font-weight: bold;
+    }
+</style>
